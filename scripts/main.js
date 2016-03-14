9ac2f47fbd19695d0fa0d0f85847c1b7e900b0d3
Martin Grossmann <mhgrossmann@yahoo.com>
2016-03-14 16:55:47 -0500

    First two exercises complete

--- scripts/main.js
+++ scripts/main.js
@@ -14,6 +14,77 @@ function average(array){
 
 console.log(average(items));
 
-Show me how to get an array of items that cost between $14.00 and $18.00 USD
+// 2. Show me how to get an array of items that cost between $14.00 and $18.00 USD
+
+// "Items that cost between $14.00 USD and $18.00 USD:"
+//   [
+//    {
+//       title: "1970s Coors Banquet Glass Beer Pitcher",
+//       ...
+//    },
+
+var costRange = items.filter(function priceRange(currentValue, index, array) {
+	if ((currentValue.price >= 14.00) && (currentValue.price <= 18.00)) {
+		return true;
+	}
+	else {
+		return false;
+	}
+});
+
+console.log('Items that cost between $14.00 and $18.00 USD: ');
+console.log(costRange.map(function title(currentValue) {
+	return currentValue.title;  
+}));
+
+
+// 3. Show me how find the item with a "GBP" currency code and print its name and price. 
+// Please console.log the one you find.
+
+var GBP = items.filter(function findGBP(currentValue, index, array) {
+	if (currentValue.currency_code === 'GBP') {
+		return true;
+	}
+	else {
+		return false;
+	}
+});
+
+console.log(GBP.map(function nameAndPrice(currentValue) {
+	
+	var name = currentValue.title;
+	var price = currentValue.price;
+	var arr1 = [name, price];
+	return arr1; 
+}));
+
+//4. Show me how to find which items are made of wood. Please console.log the ones you find.
+// if there is wood it is within the materials array located in each object.  
+//first make an array of all the material arrays with forEach
+//then check each material array for wood with filter
+
+var materials = items.forEach(function getMaterials(currentValue) {
+	return currentValue.materials;
+}
+
+console.log(materials);
+
+
+
+
+
+// //5. Show me how to find which items are made of eight or more materials. 
+// //Please console.log the ones you find.
+
+
+
+
+
+
+
+//6. Show me how to calculate how many items were made by their sellers 
+
+
+
 
 
